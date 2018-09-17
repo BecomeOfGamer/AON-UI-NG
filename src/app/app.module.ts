7b1917437bd220d719c121fc1834f406ce831c8c
Robby <ta7382@gmail.com>
2018-09-17 18:42:32 +0800

    Update player and player-life

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,10 @@ import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { SettingsComponent } from './components/settings/settings.component';
 import { PlayerComponent } from './components/player/player.component';
+import { SkillComponent } from './components/skill/skill.component';
+import { SkillcdComponent } from './components/skillcd/skillcd.component';
+import { SkillCDComponent } from './components/skill-cd/skill-cd.component';
+import { PlayerLifeComponent } from './components/player-life/player-life.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -21,6 +25,10 @@ export function HttpLoaderFactory(http: HttpClient) {
     AppComponent,
     SettingsComponent,
     PlayerComponent,
+    SkillComponent,
+    SkillcdComponent,
+    SkillCDComponent,
+    PlayerLifeComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/player-life/player-life.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-player-life',
+  templateUrl: './player-life.component.html',
+  styleUrls: ['./player-life.component.scss']
+})
+export class PlayerLifeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
