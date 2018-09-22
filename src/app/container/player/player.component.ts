289cd3413ffbc3df7f3e28995163d712826d4cfc
Robby <ta7382@gmail.com>
2018-09-23 00:17:51 +0800

    Update player components hero data

--- src/app/container/player/player.component.ts
+++ src/app/container/player/player.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HeroCharacter } from '../../models/HeroCharacter';
+import { HeroCharacterService } from '../../services/hero-character.service';
+
+import { Skill } from '../../models/Skill';
+import { SkillsService } from '../../services/skills.service';
+
+import { Buff } from '../../models/Buff';
+import { BuffsService } from '../../services/buffs.service';
+
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
@@ -7,8 +16,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayerComponent implements OnInit {
 
-  constructor() { }
+  public hero: HeroCharacter;
+  public skills: Array<Skill>;
+  public buffs: Array<Buff>;
 
-  ngOnInit() { }
+  constructor(
+    private heroCharacterService: HeroCharacterService,
+    private skillsService: SkillsService,
+    private buffsService: BuffsService,
+  ) { }
+
+  ngOnInit() {
+    this.hero = this.heroCharacterService.init();
+    this.skills = this.skillsService.init();
+    this.buffs = this.buffsService.init();
+  }
 
 }
