99eee70f8d61502e44e428a55c1c69c481f1ec5f
Robby <ta7382@gmail.com>
2018-09-23 20:23:29 +0800

    Remove unuse data

--- src/app/container/player/player.component.ts
+++ src/app/container/player/player.component.ts
@@ -6,9 +6,6 @@ import { HeroCharacterService } from '../../services/hero-character.service';
 import { Player } from '../../models/Player';
 import { PlayerService } from '../../services/player.service';
 
-import { Buff } from '../../models/Buff';
-import { BuffsService } from '../../services/buffs.service';
-
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
@@ -18,18 +15,15 @@ export class PlayerComponent implements OnInit {
 
   public hero: HeroCharacter;
   public player: Player;
-  public buffs: Array<Buff>;
 
   constructor(
     private heroCharacterService: HeroCharacterService,
     private playerService: PlayerService,
-    private buffsService: BuffsService,
   ) { }
 
   ngOnInit() {
     this.hero = this.heroCharacterService.init();
     this.player = this.playerService.init();
-    this.buffs = this.buffsService.init();
   }
 
 }
