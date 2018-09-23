70109280d973d8e53dac64ceb110a89ad89a2dc0
Robby <ta7382@gmail.com>
2018-09-23 20:03:25 +0800

    Add player model and mock data

---
+++ src/app/mock/MockPlayer.ts
@@ -0,0 +1,20 @@
+import { Player } from '../models/Player';
+
+const MockPlayer: Player = {
+  UnitName: '織田信長',
+  TeamId: 0,
+  IsAlive: 0,
+  CurrentMoveSpeed: 0,
+  CurrentMaxHP: 1000,
+  CurrentHP: 860,
+  CurrentMaxMP: 500,
+  CurrentMP: 360,
+  CurrentAttackSpeed: 120,
+  CurrentLevel: 12,
+  CurrentEXP: 3000,
+  CurrentAttack: 160,
+  CurrentArmor: 0,
+  CurrentAttackRange: 0,
+};
+
+export default MockPlayer;
---
+++ src/app/models/Player.ts
@@ -0,0 +1,16 @@
+export interface Player {
+  UnitName: string;
+  TeamId: number;
+  IsAlive: number;
+  CurrentMoveSpeed: number;
+  CurrentMaxHP: number;
+  CurrentHP: number;
+  CurrentMaxMP: number;
+  CurrentMP: number;
+  CurrentAttackSpeed: number;
+  CurrentLevel: number;
+  CurrentEXP: number;
+  CurrentAttack: number;
+  CurrentArmor: number;
+  CurrentAttackRange: number;
+}
